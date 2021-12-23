88c7b65fe77798bcfffc7507548fc6a1c44c0a8c
Marco Lombardi <63810688+LombMarc@users.noreply.github.com>
2021-12-23 19:27:30 +0100

    Update Bank_Account.py
    Update initialisation of database. Added Management interface.

--- Bank_Account.py
+++ Bank_Account.py
@@ -7,8 +7,8 @@ class Account:
 
 
 
-def addAccount(N):
-    BANK=[]
+def init_bank(N):
+    BANK=[Account("Admin","Admin",0).account]
     for i in range(N):
         name = input("Add name: ")
         pin = input("insert Pin: ")
@@ -20,12 +20,45 @@ def addAccount(N):
 #BANK=addAccount(3)
 
 BANK=[['Marco', '123', 10000.0], ['Mauro', '456', 15000.0], ['Pippo', '789', 30000.0]]
-print(BANK)
+#BANK ={"name":["admin"],"pin":["admin"],       "amount":[0]}
 
+def Add_Account_dict():
+    i=0
+    while i==0:
+        if input("Add account [y/n]? ")=="y":
+            BANK["name"].append(input("Add name: "))
+            BANK["pin"].append(input("Add pin: "))
+            BANK["amount"].append(float(input("Insert amount: ")))
+        else:
+            i=1
+
+def add_account():
+    name=input("Name: ")
+    pin=input("Pin: ")
+    amount=float(input("Amount: "))
+    temp=Account(name,pin,amount)
+    return temp.account
 
 def atm():
     name = input("Insert name: ")
     paswd=""
+    if name=="Admin" and input("Password: ") == "Admin":
+        inp=input("Management Interface. \n 1 to add account\n 2 to navigate accounts\n")
+        i=0
+        while i==0:
+            if inp=="1":
+                add_account()
+                add=input("Add another account? [y/n]")
+                if add=="y":
+                    pass
+                else:
+                    i=1
+            if inp=="2":
+                print(BANK)
+                i=1
+        print("Closing ATM...")
+        print("Restart")
+        return atm()
     for i in BANK:
         if i[0]==name:
             print("Hello, ", name)
